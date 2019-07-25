0e376c11af971ff05391039cafc0813944921cc3
グラッド・キューブ <gc00002@guratsudokyubu00002noMacBook-Pro.local>
2019-07-25 11:45:57 +0900

    Type of Typescript

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "Yuchan", age = 23, gender = "male";
-const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age} years old, you are a ${gender}`);
-};
-sayHi(name, age);
-//# sourceMappingURL=index.js.map
