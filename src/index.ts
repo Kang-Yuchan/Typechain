8a5df3c21ebcd7c94bff495bc66780968311a7dd
グラッド・キューブ <gc00002@guratsudokyubu00002noMacBook-Pro.local>
2019-07-25 16:25:58 +0900

    Validating Block Structure

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,15 @@ class Block {
         previousHash: string, 
         timestamp: number, 
         data: string
-    ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    ): string => 
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+    static validateStructure = (aBlock: Block) : boolean => 
+        typeof aBlock.index === "number" && 
+        typeof aBlock.hash === "string" && 
+        typeof aBlock.previousHash === "string" &&
+        typeof aBlock.timestamp === "number" &&
+        typeof aBlock.data === "string";
 
     constructor(
         index: number,
@@ -60,6 +68,29 @@ const createNewBlock = (data: string) : Block => {
     return newBlock;
 };
 
-console.log(createNewBlock("hello"), createNewBlock("bye bye"));
+const getHashforBlock = (aBlock: Block) : string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+
+const isBlockValid = (
+    candidateBlock : Block, 
+    previousBlock: Block
+) : boolean => {
+    if(Block.validateStructure(candidateBlock)) {
+        return false;
+    } else if(previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    } else if(previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    } else if(getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+const addBlock = (candidateBlock: Block) : void => {
+    if(isBlockValid(candidateBlock, getLatestBlock())) {
+        blockchain.push(candidateBlock);
+    }
+};
 
 export {};
